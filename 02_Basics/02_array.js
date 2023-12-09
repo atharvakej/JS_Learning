b80d4aa3f46bba5a08a28b884ff6561b88362993
Atharva Kumar <atharvados12121@gmail.com>
2023-12-09 13:07:11 +0530

    changes

--- 02_Basics/02_array.js
+++ 02_Basics/02_array.js
@@ -2,6 +2,22 @@ const arr = ["a","c","b","r"]
 
 const temp = ["assa", "asasasa" , "ewe"]
 
-arr.push(temp);
+//arr.push(temp);
 console.log(arr)
-console.log(arr[4])
+console.log(arr[3])
+const ans = arr.concat(temp);
+console.log(ans);
+
+// using spread opearator-- mostly used
+
+console.log(...arr, ...temp)
+
+// flatting a array
+
+const aray = [1,2, [12,23], [1212,[333,[233,232]]]];
+const another = aray.flat(Infinity) // depth batani hoti hai
+const anotherone = aray.flat(2) // depth batani hoti hai
+console.log(another)
+console.log(anotherone)
+
+// c
