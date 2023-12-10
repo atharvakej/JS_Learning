b48b045a413f3f7f3e09b87b41101963df52db01
Atharva Kumar <atharvados12121@gmail.com>
2023-12-10 20:00:10 +0530

    ONE

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -1,1 +1,45 @@
 // signleton
+//const tinderuser = new Object();
+// this is a singleton object
+
+
+
+
+const tinderuser1 = {};
+console.log(tinderuser)
+console.log(tinderuser1)
+
+const mysym = Symbol("key1");
+
+// object 
+const Jsuser = {
+  [mysym]: "mykey1",
+  name: "AKJ",
+  email: "atharvakjnitb@gmail.com",
+  islogged: false,
+  location: "DOS",
+};
+console.log(Jsuser.email);
+console.log(Jsuser["email"]);
+
+//for symbol use [] -- line 5
+console.log(Jsuser[mysym]);
+console.log(typeof Jsuser[mysym]);
+
+Jsuser.email = "akj@gmail.com";
+console.log(Jsuser.email);
+//Object.freeze(Jsuser);
+
+Jsuser.email = "bhagsale";
+
+console.log(Jsuser); // email didnt changed after freezing
+
+Jsuser.greeting = function () {
+  console.log("Hello JS USer");
+};
+Jsuser.greetingtwo = function () {
+  console.log(`Hello JS User ${this.name}`);
+};
+
+console.log(Jsuser.greeting());
+console.log(Jsuser.greetingtwo());
---
+++ 03_basics/functions.js
@@ -0,0 +1,6 @@
+function saymyname(){
+    console.log("AKJ")
+}
+
+
+saymyname()
