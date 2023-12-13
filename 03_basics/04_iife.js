b1fb2319e82fd92bbfabd13a3b603db06e7b41d5
Atharva Kumar <atharvados12121@gmail.com>
2023-12-14 01:35:12 +0530

    Successful

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,3 +1,4 @@
 // Immedietely Invoked Function Expression (IIFE)
 console.log("AKJ")
 console.log("Just trying to check that everything with commits are fine or not")
+console.log("Bhai Maza aaya ye sab krne me ")
