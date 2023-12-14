aa4e7c8682830eaaf1ac64d8ed7cd47d4a54618e
Atharva Kumar <atharvados12121@gmail.com>
2023-12-14 16:20:51 +0530

    Removed all Trial ones now real code only

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,6 +1,1 @@
 // Immedietely Invoked Function Expression (IIFE)
-console.log("AKJ")
-console.log("Just trying to check that everything with commits are fine or not")
-console.log("Bhai Maza aaya ye sab krne me ")
-
-console.log("Timepass kr rha hu ")
