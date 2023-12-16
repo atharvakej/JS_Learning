3483cd46e24ac3b4067e980b7bd434e9c5f2080f
Atharva Kumar <atharvados12121@gmail.com>
2023-12-16 15:42:26 +0530

    Add control flow examples and null coalescing operator

--- 04_control_flows/one.js
+++ 04_control_flows/one.js
@@ -12,4 +12,32 @@
 
 const balance = 1000
 if(balance>500) console.log("Test"), // implicit scope
-console.log("Test2");
+console.log("Test2"); 
+
+// falsy values (those are false in if condition)
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values
+
+// 'false', "0", " " , [], {}, function(){}
+
+
+
+// object empty check 
+
+const emptyobj = {}
+
+if(Object.keys(emptyobj).length===0){
+    console.log("Object is empty")
+}
+
+//  null coalescing operator (??) : null and undefined
+// security check for null and undefined values
+
+let val 
+val = 5 ??  10
+let val1
+val1 = null ?? 1010
+console.log(val);
+console.log(val1);
