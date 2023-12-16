8b25ce3d24dccb608d7deb5d2bde7c77edbf8cca
Atharva Kumar <atharvados12121@gmail.com>
2023-12-16 15:04:26 +0530

    Add control flow examples and fix formatting

--- 04_control_flows/one.js
+++ 04_control_flows/one.js
@@ -1,3 +1,15 @@
-if(condition){
-    
-}
+
+
+// === checks type too STRICT EQUAL
+
+// if(2=="2"){
+//     console.log("Executed")
+// }
+// !== same but with type too
+
+
+// shorthand notation
+
+const balance = 1000
+if(balance>500) console.log("Test"), // implicit scope
+console.log("Test2");
