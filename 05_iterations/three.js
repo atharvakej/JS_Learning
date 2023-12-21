aead4b4ea8643ec71d49780ffd04708a1e9ea8c6
Atharva Kumar <atharvados12121@gmail.com>
2023-12-21 23:33:13 +0530

    Refactor code to use more descriptive variable names and add iteration examples

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,7 +1,21 @@
 const arr = [1, 2, 3, 2, 3];
 
-for (const i of arr) {
-  console.log(i);
+for (const num of arr) {
+  console.log(num);
 }
 
-console.log("Prep for Amazon Interview so busy");
+const greetings = "Hello_World";
+
+for (const it of greetings) {
+  console.log(`each char is ${it}`);
+}
+
+const mapio = new Map();
+
+mapio.set("FR", "france");
+mapio.set("FR", "france");
+mapio.set("FRr", "ffrance");
+
+for (const [k, value] of mapio) {
+  console.log(k, ":-", value);
+}
