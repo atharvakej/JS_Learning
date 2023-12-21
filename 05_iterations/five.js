caf3c6ada5d24d1dc532c1145b51198cea464401
Atharva Kumar <atharvados12121@gmail.com>
2023-12-21 23:56:20 +0530

    Refactor code in five.js to use arrow functions and add new array iteration example

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,10 +1,33 @@
-const coding = ["Js", "php", "disco"]
+const coding = ["Js", "php", "disco"];
 // coding.forEach( function (item){
 //     console.log(item);
 // })
 
-function printme(item){
-    console.log(item);
+function printme(item) {
+  console.log(item);
 }
 
-coding.forEach(printme)
+//coding.forEach(printme)
+
+// coding.forEach((item,ind,ar)=>{
+//     console.log(item,ind,ar)
+// })
+
+const arr = [
+  {
+    languagename: "JavaScript",
+    syntax: "js",
+  },
+  {
+    languagename: "Java",
+    syntax: "java",
+  },
+  {
+    languagename: "Python",
+    syntax: "py",
+  },
+];
+
+arr.forEach((item) => {
+  console.log(item.syntax);
+});
